ac9e3b300958a035b3723cf6f4fd2f43b00e8cc9
haoyao <15298384543eBay!>
2020-01-09 17:36:12 +0800

    -start of blog

---
+++ src/main/java/com/blog/yaoBlog/YaoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.yaoBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaoBlogApplication.class, args);
+	}
+
+}
